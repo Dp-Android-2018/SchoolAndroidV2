c09328b6f5d48bf9db74eaa2502518086f9a75d0
Bob Boborama <youremail@who.com>
2018-12-22 11:10:11 +0200

    no change

--- src/main/java/dp/schoolandroid/service/repository/remotes/ApiInterfaces.java
+++ src/main/java/dp/schoolandroid/service/repository/remotes/ApiInterfaces.java
@@ -21,6 +21,7 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ApiInterfaces {
 
