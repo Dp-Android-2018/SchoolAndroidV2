6cee446bf3b5c6350f298fec7a26e1ff381283ef
bahaa_gabal <bahaa.gabal@yahoo.com>
2018-12-29 14:04:47 +0200

    Clean Architecture

---
+++ src/main/java/dp/schoolandroid/Utility/utils/CustomUtils.java
@@ -0,0 +1,28 @@
+package dp.schoolandroid.Utility.utils;
+
+import android.app.Application;
+import android.content.Context;
+
+import dp.schoolandroid.Utility.SharedPreferenceHandler;
+import dp.schoolandroid.service.model.response.teacherresponse.TeacherResponse;
+import dp.schoolandroid.service.repository.remotes.GetApiInterfaces;
+import dp.schoolandroid.service.repository.remotes.GetSharedPreference;
+
+public class CustomUtils {
+    private SharedPreferenceHandler prefrenceUtils;
+
+    public CustomUtils(Application application) {
+        this.prefrenceUtils = GetSharedPreference.getInstance().getSharedPreference(application);
+    }
+
+    public TeacherResponse getSavedTeacherData() {
+        return (TeacherResponse) prefrenceUtils.getSavedObject(ConfigurationFile.SharedPrefConstants.SHARED_PREF_NAME, TeacherResponse.class);
+    }
+    public void saveTeacherDataToPrefs(TeacherResponse data) {
+        prefrenceUtils.saveObjectToSharedPreferences(ConfigurationFile.SharedPrefConstants.SHARED_PREF_NAME, data);
+    }
+
+    public void clearSharedPref() {
+        prefrenceUtils.clearToken();
+    }
+}
